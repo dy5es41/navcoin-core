16aa5617e132fb635b1bf6109c6c05af789b5f81
dy5es41 <dy5es41@protonmail.com>
2019-01-31 10:04:41 +1300

    new donation redesign, this is not going to be implemented

---
+++ src/qt/forms/cfund_donating.cpp
@@ -0,0 +1,6 @@
+#include "cfund_donating.h"
+
+CFund_Donating::CFund_Donating()
+{
+
+}
---
+++ src/qt/forms/cfund_donating.h
@@ -0,0 +1,11 @@
+#ifndef CFUND_DONATING_H
+#define CFUND_DONATING_H
+
+
+class CFund_Donating
+{
+public:
+    CFund_Donating();
+};
+
+#endif // CFUND_DONATING_H
